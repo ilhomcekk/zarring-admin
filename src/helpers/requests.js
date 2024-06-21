e348e31ce17a8db2bdf6fc324339df186b1fec8a
Ilhom <nasriddinov2004@gmail.com>
2024-06-22 01:32:25 +0500

    adsads

--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -6,21 +6,13 @@ export const requests = {
   // PRODUCTS
   fetchProduct: (params) => $api.get(`${API_URL}/product/`, { params }),
   createProduct: (params) => $api.post(`${API_URL}/product/add`, formData(params)),
-  editProduct: (params) => $api.post(`${API_URL}/product/update`, formData(params)),
-  deleteProduct: (id) => $api.delete(`${API_URL}/product/delete/${id}`),
   // CATEGORY
   fetchCategory: (params) => $api.get(`${API_URL}/category`, { params }),
   createCategory: (params) => $api.post(`${API_URL}/category/add`, formData(params)),
-  editProduct: (params) => $api.post(`${API_URL}/product/update`, formData(params)),
-  deleteProduct: () => $api.get(`${API_URL}/product/delete`),
   // ORDER
-  fetchProducts: (params) => $api.get(`${API_URL}/product`, { params }),
-  createProduct: (params) => $api.post(`${API_URL}/product/add`, formData(params)),
-  editProduct: (params) => $api.post(`${API_URL}/product/update`, formData(params)),
-  deleteProduct: () => $api.get(`${API_URL}/product/delete`),
+  // fetchProducts: (params) => $api.get(`${API_URL}/product`, { params }),
+  // createProduct: (params) => $api.post(`${API_URL}/product/add`, formData(params)),
   // REPORTS
-  fetchProducts: (params) => $api.get(`${API_URL}/product`, { params }),
-  createProduct: (params) => $api.post(`${API_URL}/product/add`, formData(params)),
-  editProduct: (params) => $api.post(`${API_URL}/product/update`, formData(params)),
-  deleteProduct: () => $api.get(`${API_URL}/product/delete`),
+  // fetchProducts: (params) => $api.get(`${API_URL}/product`, { params }),
+  // createProduct: (params) => $api.post(`${API_URL}/product/add`, formData(params)),
 }
