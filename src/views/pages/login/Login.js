766a4e616c66b028915ab6609d83a70404a65678
ilhom Nasriddinov <nasriddinov2004@gmail.com>
2025-05-15 15:52:43 +0500

    +

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -88,7 +88,7 @@ const Login = () => {
                   </CForm>
                 </CCardBody>
               </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
+              <CCard className="login-box text-white bg-primary py-5" style={{ width: '44%' }}>
                 <CCardBody className="text-center">
                   <div>
                     <h2>Zarring админка</h2>
